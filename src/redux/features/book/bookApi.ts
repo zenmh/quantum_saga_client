3cda464095f44759731adee12f58130f51e17af3
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-08-15 12:02:48 +0600

    All endpoints are made for book api

---
+++ src/redux/features/book/bookApi.ts
@@ -0,0 +1,43 @@
+import api from "../../apiSlice";
+
+const bookApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    getBooks: builder.query({
+      query: () => "/books",
+    }),
+    getBook: builder.query({
+      query: (id) => `/books/${id}`,
+    }),
+    createBook: builder.mutation({
+      query: (data) => ({
+        url: `/books`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    updateBook: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `/books/${id}`,
+        method: "PATCH",
+        body: data,
+      }),
+    }),
+    deleteBook: builder.mutation({
+      query: (id) => ({
+        url: `/books/${id}`,
+        method: "DELETE",
+        body: null,
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetBookQuery,
+  useGetBooksQuery,
+  useCreateBookMutation,
+  useUpdateBookMutation,
+  useDeleteBookMutation,
+} = bookApi;
+
+export default bookApi;
---
+++ src/redux/features/book/bookSlice.ts
@@ -0,0 +1,3 @@
+const bookSlice = () => {};
+
+export default bookSlice;
