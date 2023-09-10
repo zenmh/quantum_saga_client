6acda59fde662cc81fb2e3da2027db4c86ea67e5
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-09-10 14:43:18 +0600

    Required all things are done

--- src/redux/apiSlice.ts
+++ src/redux/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
+    baseUrl: "https://quantum-saga.vercel.app/api/v1",
   }),
   endpoints: () => ({}),
   tagTypes: [
